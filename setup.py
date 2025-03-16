9c91442f6e5f0cf7fac25312a459f5778a92a4b1
Ahmedalanee <32652568+Ahmedalanee@users.noreply.github.com>
2025-03-16 21:43:24 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="sidebar_hider",
-    version="0.0.1",
-    description="Custom App to Hide Sidebar in ERPNext",
+    version="0.0.1",  # Must match __init__.py
+    description="An ERPNext app to auto-hide the sidebar.",
     author="Your Name",
     author_email="your_email@example.com",
     packages=find_packages(),
