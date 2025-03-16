72d16983de8053d2a943a5bd4bc26412c425ec6e
Ahmedalanee <32652568+Ahmedalanee@users.noreply.github.com>
2025-03-16 21:10:17 +0300

    Update hooks.py

--- sidebar_hider/hooks.py
+++ sidebar_hider/hooks.py
@@ -1,1 +1,9 @@
-app_include_css = "/assets/sidebar_hider/css/sidebar_hide.css"
+app_name = "sidebar_hider"
+app_title = "Sidebar hider"
+app_publisher = "Your Name"
+app_description = "An ERPNext app to hide the sidebar automatically."
+app_email = "your_email@example.com"
+app_license = "MIT"
+
+# Include JavaScript and CSS globally
+app_include_css = "/assets/sidebar_fixer/css/sidebar_fix.css"
